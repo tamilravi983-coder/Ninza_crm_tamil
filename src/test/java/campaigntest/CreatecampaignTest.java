159c8227fb740fd7b5cd6c55e7ab75d3024e02a2
SABARI <SABARI@SABARI-PC>
2025-11-10 20:56:31 +0530

    change

--- src/test/java/campaigntest/CreatecampaignTest.java
+++ src/test/java/campaigntest/CreatecampaignTest.java
@@ -45,6 +45,7 @@ public class CreatecampaignTest extends Baseclass {
 	createcampaignpage.getCampaignnameTF().sendKeys(CAMPAIGN_NAME);
 	createcampaignpage.getTargetsizeTF().sendKeys(TARGET_SIZE);
 	createcampaignpage.getCreatecampaignbtn().click();
+	System.out.println("hi");
 	
         Homepage homepage = new Homepage(driver);
 	WebElement toastmsg = homepage.getToastmsg();
